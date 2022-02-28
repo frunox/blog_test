10be2d536a1f86c234603912aa97e78ab62b50db
frunox <jrcannon@msn.com>
2022-02-28 09:50:26 -0600

    fix template_not_found/2 errors in views

--- lib/blog_test_web/views/layout_view.ex
+++ lib/blog_test_web/views/layout_view.ex
@@ -4,4 +4,8 @@ defmodule BlogTestWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def template_not_found(template, _assigns) do
+      Phoenix.Controller.status_message_from_template(template)
+  end
 end
